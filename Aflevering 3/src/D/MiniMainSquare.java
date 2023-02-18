a405581dccc99bb47d5f738b63e69d557dfad254
Amalie <Amal956e@stud.kea.dk>
2023-02-18 16:31:34 +0100

    Færdig

--- Aflevering 3/src/D/MiniMainSquare.java
+++ Aflevering 3/src/D/MiniMainSquare.java
@@ -5,14 +5,14 @@ import java.util.Collections;
 
 public class MiniMainSquare {
     public static void ExerciseD(){
-        Square square1 = new Square(5, 6);
-        Square square2 = new Square(2, 1);
+        Square square1 = new Square(10, 10);
+        Square square2 = new Square(10, 5);
         System.out.println(square1.compareTo(square2));
 
         ArrayList<Square> squareList = new ArrayList<>();
         squareList.add(square1);
         squareList.add(square2);
         Collections.sort(squareList);
-        System.out.println("Den med den største omkreds står først: " + squareList);
+        System.out.println("Den med den mindste omkreds står først: " + squareList);
     }
 }
